323e9a78a14498478c77bed6f681d010ec431349
Juan Ante <juanantec@unicauca.edu.co>
2024-01-25 23:15:48 -0500

    relocate

--- Backend/src/controllers/foodController.ts
+++ Backend/src/controllers/foodController.ts
@@ -29,22 +29,22 @@ export class FoodController {
         if (foodDetails.foodPortions) {
           const foodPortions = foodDetails.foodPortions;
           foodData.totalServings = foodPortions.length;
-          console.log("\n");
+          //console.log("\n");
           for (let index = 0; index < foodData.totalServings; index++) {
             const element = foodPortions[index];
             if (element.modifier == undefined) {
-              console.log("\t", element.gramWeight + " " + element.measureUnit.name);
+              //console.log("\t", element.gramWeight + " " + element.measureUnit.name);
               foodData.servings.push([element.gramWeight, element.measureUnit.name]);
             } else{
-              console.log("\t", element.gramWeight + " " + element.modifier + " " + element.measureUnit.name);
+              //console.log("\t", element.gramWeight + " " + element.modifier + " " + element.measureUnit.name);
               foodData.servings.push([element.gramWeight, element.modifier + " " + element.measureUnit.name]);
             }
           }
-          console.log('Servings:', foodData.totalServings);
+          //console.log('Servings:', foodData.totalServings);
         } else {
           foodData.totalServings = 1;
-          console.log('Servings: ', foodData.totalServings);
-          console.log("\t", "100 g");
+          //console.log('Servings: ', foodData.totalServings);
+          //console.log("\t", "100 g");
           foodData.servings.push(['100', 'g'])
         }
 
@@ -61,28 +61,28 @@ export class FoodController {
           if (element.nutrient.isNutrientLabel) {
             category = element.nutrient.name
             categoryList.push(category);
-            console.log("\n", category);
+            //console.log("\n", category);
           } else if (element.amount || (element.amount == 0 && !element.loq)) {
-            console.log("\t", element.nutrient.name, element.amount, element.nutrient.unitName)
+            //console.log("\t", element.nutrient.name, element.amount, element.nutrient.unitName)
             foodData["nutrients"].push([category, `${element.nutrient.name}`, `${element.amount}`, `${element.nutrient.unitName}`]);
           }
           else if (element.loq) {
-            console.log("\t", element.nutrient.name, `<${element.loq}`, element.nutrient.unitName)
+            //console.log("\t", element.nutrient.name, `<${element.loq}`, element.nutrient.unitName)
             foodData["nutrients"].push([category, `${element.nutrient.name}`, `${element.loq}`, `<${element.nutrient.unitName}`]);
           }
         }
         if (categoryList.length == 0)
           throw new Error("\nLas categorias no se cargaron correctamente");
         foodData.totalNutrients -= categoryList.length;
-        console.log('\nNutrients: ', foodData.totalNutrients);
+        //console.log('\nNutrients: ', foodData.totalNutrients);
 
-        console.log("------------------------------------------------------------------");
-        console.log(foodData);
-        return res.json({foodDetails});
+        //console.log("------------------------------------------------------------------");
+        //console.log(foodData);
+        return res.json({foodData});
       }
 
     } catch (error: any) {
-      res.status(500).json({message: error.message, error: error});
+      res.status(500).json({error: error.message});
     }
   }
 }
